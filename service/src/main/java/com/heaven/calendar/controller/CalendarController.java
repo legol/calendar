07c9e71b2b7c7050e5e806afbac605eedb225706
chenjie3 <chenjie3@kingsoft.com>
2015-09-23 12:14:12 +0800

    added a test page

--- service/src/main/java/com/heaven/calendar/controller/CalendarController.java
+++ service/src/main/java/com/heaven/calendar/controller/CalendarController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,8 +16,9 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class CalendarController {
     @RequestMapping("/test")
-    @ResponseBody
+    //@ResponseBody
     public Object test(HttpServletRequest request, HttpServletResponse response) {
-        return "hello calendar & spring mvc!";
+        return new ModelAndView("index.jsp");
+        //return "hello calendar & spring mvc!";
     }
 }
